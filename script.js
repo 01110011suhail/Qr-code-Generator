93f404c2b7b725d4f51356fa8681b505e26cfd5a
01110011suhail <sha433415@gmail.com>
2024-01-29 19:10:55 +0530

    updates2

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-const qrText =document.getElementById('qr-text');
-const Sizes= document.getElementById('sizes');
-
-const generatebtn= document.getElementById('generatebtn');
+const qrText = document.getElementById('qr-text');
+const Sizes = document.getElementById('Sizes');  
+const generatebtn = document.getElementById('generatebtn');
 const downloadbtn = document.getElementById('downloadbtn');
+const qrContainer = document.querySelector('.qr-body');
+let size = Sizes.value;
 
-const qrContainer = document.querySelector('qr-body');
-
-generatebtn.addEventListener('click',(e)=>{
-    e.preventDefault;
-    genrateCode();
+generatebtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    isEmptyInput();
+    generateCode();
+    
+    
 });
-function genrateCode(){
-    qrContainer.innerHTML="";
-    new QrCode(qrContainer, {
-        text:qrText.value,
-        height :size,
-        width:size,
+
+Sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    isEmptyInput();
+});
+function isEmptyInput(){
+    if(qrText.value.length > 0){
+       
+
+    }
+    else{
+        alert("enter the text or url to generate");
+    }
+
+}
+
+function generateCode() {
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {  
+        text: qrText.value,
+        height: size,
+        width: size,
         colorLight: "#fff",
-        colorDark : "#000",
-
-    })
-
+        colorDark: "#000",
+    });
 }
